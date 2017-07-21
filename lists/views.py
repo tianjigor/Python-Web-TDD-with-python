fad364334fb32e17a096e944ad2a0aff5612dc87
tianjigor <541878782@qq.com>
2017-07-21 20:42:47 +0800

    6-2~6-5 new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -7,6 +7,13 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
+
+
+def view_list(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})

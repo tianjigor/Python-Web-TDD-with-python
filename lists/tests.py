c98d372a33b19438c5a065bbf2911f8434cb0300
tianjigor <541878782@qq.com>
2017-07-21 17:32:53 +0800

    5-6~5-9 Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -29,20 +29,41 @@ class HomePageTest(TestCase):
     #     self.assertEqual(response.content.decode(), excepted_html)
 
 
-    # def test_home_page_can_save_a_POST_request(self):
-    #     request = HttpRequest()
-    #     request.method = 'POST'
-    #     request.POST['item_text'] = 'A new list item'
-    #
-    #     response = home_page(request)
-    #     self.assertIn('A new list item', response.content.decode())
-    #     expected_html = render_to_string(
-    #         'home.html',
-    #         {'new_item_text': 'A new list item'}
-    #     )
-    #     # print(expected_html)
-    #     # print(response.content.decode())
-    #     self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 from lists.models import Item
@@ -65,3 +86,5 @@ class ItemModelTest(TestCase):
         second_saved_item = save_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request, 'home.html')
-
-    # return render_to_response('home.html')
-
-    # return render(request, 'home.html', {
-    #     'new_item_text': request.POST['item_text'],
-    # })
-
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})

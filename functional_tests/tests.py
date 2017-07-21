269554409adace85938d0b1f9c31f88529f4aee4
tianjigor <541878782@qq.com>
2017-07-21 17:45:50 +0800

    6-1 make functional_tests an app, use LiveServerTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+import time
+
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get(self.live_server_url)
+
+
+        self.fail('Finish the test!')

b0db1b35606cfc7d4b3384200ccab02832705172
tianjigor <541878782@qq.com>
2017-07-30 23:43:47 +0800

    10.5.1 Use get_absolute_url on List model to DRY urls in views

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -36,5 +36,6 @@ class ItemValidationTest(FunctionalTest):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Make tea')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')

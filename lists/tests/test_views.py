b5d49a39a742bdfb00ff5b4257227dcfd3310b7d
tianjigor <541878782@qq.com>
2017-08-02 10:12:18 +0800

    11.3 在处理POST请求的视图中使用这个表单

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,7 @@ from lists.views import home_page
 from django.http import HttpRequest
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 from django.template.loader import render_to_string
 # Create your tests here.
 
@@ -107,12 +107,20 @@ class NewListTest(TestCase):
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id))
 
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
+    def test_for_invalid_input_renders_home_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_LIST_ERROR))
+
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
     def test_invalid_list_items_arent_saved(self):

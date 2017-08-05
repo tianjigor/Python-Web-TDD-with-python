20f24c13418e65a63dffe025025d7947fc06249d
tianjigor <541878782@qq.com>
2017-08-05 20:14:38 +0800

    12.3 处理唯一性验证的复杂表单

--- lists/forms.py
+++ lists/forms.py
@@ -30,3 +30,22 @@ class ItemForm(forms.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_LIST_ERROR}
         }
+
+
+# 12.3
+from django.core.exceptions import ValidationError
+
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_error(e)

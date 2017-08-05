f0fcc01cd17c83ce15d620f3a98c4137fac7ffbb
tianjigor <541878782@qq.com>
2017-08-05 16:28:43 +0800

    12.2 在视图层试验待办事项重复验证 运

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,8 @@ import time
 
 
 class ItemValidationTest(FunctionalTest):
+
+    @skip
     def test_cannot_add_empty_list_items(self):
         # 伊迪丝访问首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
@@ -39,3 +41,24 @@ class ItemValidationTest(FunctionalTest):
         time.sleep(1)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+
+    def test_cannot_add_duplicate_items(self):
+        # 伊迪丝访问首页，新建一个清单
+        self.browser.get(self.server_url)
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy wellies')
+        time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy wellies')
+        # 她不小心输入了一个重复的待办事项
+        inputbox = self.get_item_input_box()
+        inputbox.send_keys('Buy wellies')
+        time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # 她看到一条有帮助的错误消息
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
